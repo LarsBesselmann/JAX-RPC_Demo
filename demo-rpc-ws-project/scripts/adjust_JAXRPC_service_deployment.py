f572b5f4bd5aabf9eb9b8920478cd53f524c34a9
Lars Besselmann <lars.besselmann@de.ibm.com>
2025-12-08 15:50:41 +0100

    Update scripts

--- demo-rpc-ws-project/scripts/adjust_JAXRPC_service_deployment.py
+++ demo-rpc-ws-project/scripts/adjust_JAXRPC_service_deployment.py
@@ -1,3 +1,3 @@
 # Map the DemoRPC-1_0_0_war to server1 and the web server
 AdminApp.edit('DemoRPC-1_0_0_war', '[ -MapModulesToServers [[ "Demo RPC Web Service" DemoRPC-1.0.0.war,WEB-INF/web.xml WebSphere:cell=rhel9-baseCell01,node=rhel9-baseNode01,server=server1+WebSphere:cell=rhel9-baseCell01,node=rhel9-baseNode01,server=webserver1 ]]]' )
- 
+AdminConfig.save()
