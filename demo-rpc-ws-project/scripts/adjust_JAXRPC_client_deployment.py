bb2124ef25cf6cfaaa247110f3d6339a7e79967b
Lars Besselmann <lars.besselmann@de.ibm.com>
2025-12-08 15:46:45 +0100

    Update scripts

--- demo-rpc-ws-project/scripts/adjust_JAXRPC_client_deployment.py
+++ demo-rpc-ws-project/scripts/adjust_JAXRPC_client_deployment.py
@@ -1,8 +1,5 @@
-# Map the DemoRPCClient-1_0_0_war client application to server2
-AdminApp.edit('DemoRPCClient-1_0_0_war', '[ -MapModulesToServers [[ DemoRPCClient-1.0.0.war DemoRPCClient-1.0.0.war,WEB-INF/web.xml WebSphere:cell=rhel9-baseCell01,node=rhel9-baseNode01,server=webserver1+WebSphere:cell=rhel9-baseCell01,node=rhel9-baseNode02,server=server2 ]]]' )
-
 # Adjust the DemoRPCClient-1_0_0_war context root to "/DemoRPCClient"
 AdminApp.edit('DemoRPCClient-1_0_0_war', '[ -CtxRootForWebMod [[ DemoRPCClient-1.0.0.war DemoRPCClient-1.0.0.war,WEB-INF/web.xml /DemoRPCClient ]]]' )
- 
-# Optional: Map the DemoRPCClient-1_0_0_war to server2 and the web server
-# AdminApp.edit('DemoRPCClient-1_0_0_war', '[ -MapModulesToServers [[ DemoRPCClient-1.0.0.war DemoRPCClient-1.0.0.war,WEB-INF/web.xml WebSphere:cell=rhel9-baseCell01,node=rhel9-baseNode01,server=webserver1+WebSphere:cell=rhel9-baseCell01,node=rhel9-baseNode02,server=server2 ]]]' )
+
+# Map the DemoRPCClient-1_0_0_war to server2 and the web server
+AdminApp.edit('DemoRPCClient-1_0_0_war', '[ -MapModulesToServers [[ DemoRPCClient-1.0.0.war DemoRPCClient-1.0.0.war,WEB-INF/web.xml WebSphere:cell=rhel9-baseCell01,node=rhel9-baseNode01,server=webserver1+WebSphere:cell=rhel9-baseCell01,node=rhel9-baseNode02,server=server2 ]]]' )
